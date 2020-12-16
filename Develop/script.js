e48301e461ab831af2a3ca7d7e3c23d3c77e7fc5
kate <katy.madden99@gmail.com>
2020-12-16 13:36:52 +0000

    Added in a function that runs when generate button is clicked, so far it asks for the length of the password and if you want to include lowercase characters

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,16 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var numChar = prompt("How many characters (between 8-128) do you want your password to be?");
+  // If user enters number >= 8 or <= 128, then run this code
+  if (numChar >= 8 && numChar <= 128) {
+    var lwCase = confirm("Do you want to include lowercase characters?")
+  }
+  else {
+    alert("Your password must be between 8 and 128 characters only.");
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
