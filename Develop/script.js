afd460359c3d1b88000b70eeb4cf8fa9214a4e72
kate <katy.madden99@gmail.com>
2020-12-17 19:57:08 +0000

    Added in a function that adds all the user's preferences of a password to a string then that string is used to generate a random password, when button clicked again it generates a new password.

--- Develop/script.js
+++ Develop/script.js
@@ -2,21 +2,30 @@
 var generateBtn = document.querySelector("#generate");
 var password = "";
 var pwEl = document.querySelector("#password");
+var lwCaseQ = false;
+var upCaseQ = false;
+var numQ = false;
+var spcCharQ = false;
+var trueCount = 0;
+var numChar = 0;
+var userString = "";
 
 // Write password to the #password input
 function writePassword() {
+  resetQuestions();
   password = "";
   pwEl.value = password;
   generatePassword();
-  pwEl.value = password;
-
+  // pwEl.value = password;
+  showPassword();
+  userString = "";
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var numChar = prompt("How many characters (between 8-128) do you want your password to be?");
+  numChar = prompt("How many characters (between 8-128) do you want your password to be?");
   // If user enters number >= 8 or <= 128, then run this code
   if (numChar >= 8 && numChar <= 128) {
     startQuestions();
@@ -28,54 +37,81 @@ function generatePassword() {
 }
 
 function startQuestions() {
-  lowerCase();
-  upperCase();
-  numberGen();
-  specialChar();
-
+  lwCaseQ = confirm("Do you want to include lowercase characters?");
+  if (lwCaseQ == true) {
+    lowerCase();
+    trueCount += 2;
+  }
+  upCaseQ = confirm("Do you want to include uppercase characters?");
+  if (upCaseQ == true) {
+    upperCase();
+    trueCount += 2;
+  }
+  numQ = confirm("Do you want to include numbers?");
+  if (numQ == true) {
+    numberGen();
+    trueCount += 2;
+  }
+  var spcCharQ = confirm("Do you want to include special characters?");
+  if (spcCharQ == true) {
+    specialChar();
+    trueCount += 2;
+  }
 }
 
 function lowerCase() {
   var lwcse = "abcdefghijklmnopqrstuvwxyz";
-  var lwCaseQ = confirm("Do you want to include lowercase characters?");
-  if (lwCaseQ == true) {
-    for (var x = 0; x < 2; x++) {
-      var i = Math.floor(Math.random() * lwcse.length);
-      password += lwcse.charAt(i);
-    }
+  userString += lwcse;
+  for (var x = 0; x < 2; x++) {
+    var i = Math.floor(Math.random() * lwcse.length);
+    password += lwcse.charAt(i);
   }
 }
 
 
 function upperCase() {
   var upcse = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var upCaseQ = confirm("Do you want to include uppercase characters?");
-  if (upCaseQ == true) {
-    for (var x = 0; x < 2; x++) {
-      var i = Math.floor(Math.random() * upcse.length);
-      password += upcse.charAt(i);
-    }
+  userString += upcse;
+  for (var x = 0; x < 2; x++) {
+    var i = Math.floor(Math.random() * upcse.length);
+    password += upcse.charAt(i);
   }
 }
 
 function numberGen() {
   var nums = "0123456789";
-  var numQ = confirm("Do you want to include numbers?");
-  if (numQ == true) {
-    for (var x = 0; x < 2; x++) {
-      var i = Math.floor(Math.random() * nums.length);
-      password += nums.charAt(i);
-    }
+  userString += nums;
+  for (var x = 0; x < 2; x++) {
+    var i = Math.floor(Math.random() * nums.length);
+    password += nums.charAt(i);
   }
 }
 
 function specialChar() {
   var spcChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var spcCharQ = confirm("Do you want to include special characters?");
-  if (spcCharQ == true) {
-    for (var x = 0; x < 2; x++) {
-      var i = Math.floor(Math.random() * spcChar.length);
-      password += spcChar.charAt(i);
+  userString += spcChar;
+  for (var x = 0; x < 2; x++) {
+    var i = Math.floor(Math.random() * spcChar.length);
+    password += spcChar.charAt(i);
+  }
+}
+
+function resetQuestions() {
+  lwCaseQ = false;
+  upCaseQ = false;
+  numQ = false;
+  spcCharQ = false;
+}
+
+function showPassword() {
+  debugger;
+  numChar = parseInt(numChar, 10);
+  while (numChar !== password.length) {
+    for (var x = 0; x < 1; x++) {
+      var i = Math.floor(Math.random() * userString.length);
+      password += userString.charAt(i);
+      x += 1;
     }
   }
+  pwEl.value = password;
 }
